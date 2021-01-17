9ea08646e6a51dc66c09783994494f13c15e4e56
puneetdwivedi <pdwivedi294@gmail.com>
2021-01-17 23:22:24 +0530

    added insertion sort

---
+++ insertionsort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int arr[7]={1,9,7,3,5,8,0};
+    for(int i=0;i<7;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=key;
+    }    
+    for(int i=0;i<7;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
